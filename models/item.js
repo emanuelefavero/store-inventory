e0d56b2a8108aecfb333b365341a197e6ec62fea
emanuelefavero <emafaux89@gmail.com>
2022-10-17 17:44:17 +0200

    add item schema

--- models/item.js
+++ models/item.js
@@ -2,7 +2,33 @@ const mongoose = require('mongoose')
 
 const Schema = mongoose.Schema
 
-const ItemSchema = new Schema({})
+const ItemSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+    minLength: 3,
+    maxLength: 100,
+  },
+  description: {
+    type: String,
+    minLength: 3,
+    maxLength: 1000,
+  },
+  price: {
+    type: Number,
+    required: true,
+    min: 0,
+  },
+  category: {
+    type: Schema.Types.ObjectId,
+    ref: 'Category',
+    // required: true,
+  },
+  dateAdded: {
+    type: Date,
+    default: Date.now,
+  },
+})
 
 // Virtual for book's URL
 ItemSchema.virtual('url').get(function () {
